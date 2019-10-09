96e8cd57d2ba7d25957a0d3ae3fdd678aaf57beb
Matt Bretl <mpbretl@gmail.com>
2019-10-09 10:50:46 -0500

    style: lint all .js files

--- index.js
+++ index.js
@@ -8,11 +8,7 @@ module.exports = function PgOrderByMultiColumnIndexPlugin(
     // Check dependencies
     if (!build.versions) {
       throw new Error(
-        `Plugin ${pkg.name}@${
-          pkg.version
-        } requires graphile-build@^4.1.0 in order to check dependencies (current version: ${
-          build.graphileBuildVersion
-        })`
+        `Plugin ${pkg.name}@${pkg.version} requires graphile-build@^4.1.0 in order to check dependencies (current version: ${build.graphileBuildVersion})`
       );
     }
     const depends = (name, range) => {
